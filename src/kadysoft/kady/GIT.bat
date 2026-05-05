e570e66e173b91defce04cf38fb6ed06f6390ce3
Ahmed Elkady <ahmed.elkady@Kadysoft.tandc.local>
2026-05-05 15:01:58 +0300

    Auto update $(date)

--- src/kadysoft/kady/GIT.bat
+++ src/kadysoft/kady/GIT.bat
@@ -1,5 +1,11 @@
 cd C:\Users\Ahmed.ElKady\Documents\NetBeansProjects\RecipeMaker
-git pull origin master
+
+git checkout master
+
+git pull --rebase origin master
+
 git add .
-git diff --cached --quiet || git commit -m "Update some data"
+
+git diff --cached --quiet || git commit -m "Auto update $(date)"
+
 git push origin master
